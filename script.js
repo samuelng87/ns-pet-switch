f5e3f3fdcd76a328fe503741ade1e4b7ac1e5529
Samuel <59765994+samuelng87@users.noreply.github.com>
2020-09-16 00:37:56 +1000

    nav
    gif

--- script.js
+++ script.js
@@ -21,3 +21,40 @@ downBtn.onclick = function () {
 }
 
 
+var i = 0; // start point 
+var images = []
+var time = 5000;
+
+
+// Image List 
+
+images [0] = 'main-1.gif';
+images [1] = 'main-2.gif';
+images [2] = 'main-3.gif';
+images [3] = 'main4.gif';
+
+
+// change img 
+
+function changeImg() {
+    document.slide.src = images[i];
+
+if ( i < images.length - 1){
+i++ ;  
+} else {
+    i = 0; 
+}
+
+setTimeout("changeImg()", time);   
+}
+
+
+window.onload = changeImg; 
+
+
+function printNumber (num) {
+    return num;
+}
+
+
+printNumber((2020,2021))
