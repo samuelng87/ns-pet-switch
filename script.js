b0c2a0a31aa0b8ab075855d922fc03d3658146c2
Samuel <59765994+samuelng87@users.noreply.github.com>
2020-09-18 18:34:11 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 var circle = document.getElementById('circle')
 var upBtn = document.getElementById('upBtn')
 var downBtn = document.getElementById('downBtn')
@@ -64,21 +84,3 @@ function printNumber (num) {
 
 printNumber((2020,2021))
 
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
