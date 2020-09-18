bd6b97b2350c824b4955684dda70d05bf7627355
Samuel <59765994+samuelng87@users.noreply.github.com>
2020-09-18 19:00:46 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 var mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -12,7 +11,6 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
